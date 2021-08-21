4349b3a9a0dca4eb7c2e56cb92daa97b9a26855a
khazin <khazin316@gmail.com>
2021-08-21 21:24:36 +0800

    done mobile layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        redhatSans: [
+        'Red Hat Display',
+        'sans-serif'
+        ]
+      },
+      fontWeight: {
+        sm: '500',
+        md: '700',
+        lg: '900'
+      },
+      colors: {
+        paleBlue: 'hsl(225, 100%, 94%)',
+        brightBlue: 'hsl(245, 75%, 52%)',
+        veryPaleBblue: 'hsl(225, 100%, 98%)',
+        desaturatedBlue: 'hsl(224, 23%, 55%)',
+        darkBlue: 'hsl(223, 47%, 23%)'
+      },
+      screens: {
+        tablet: '768px',
+        desktop: '1366px'
+      }
+    },
   },
   variants: {
     extend: {},
